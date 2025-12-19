44cd15e9b1b21c4a5b5386a133ba094398677c54
Killernoob1163 <lileon2008@gmail.com>
2025-12-19 09:32:24 +0100

    Add project files.

---
+++ BlackJack.Lib/CardDeck.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack.Lib
+{
+    public class CardDeck
+    {
+        public Card[] Cards { get; }
+        public CardDeck()
+        {  
+            Cards= new Card[52];
+            foreach(CardSuit suit in )
+        }
+    }
+}
