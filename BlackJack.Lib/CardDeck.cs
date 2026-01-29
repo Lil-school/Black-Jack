8ab1c3ff0213d2e1517146cd74aac0e4a6d34153
Killernoob1163 <lileon2008@gmail.com>
2026-01-29 16:09:56 +0100

    first summarys and test for vs 2026

--- BlackJack.Lib/CardDeck.cs
+++ BlackJack.Lib/CardDeck.cs
@@ -6,10 +6,17 @@ using System.Threading.Tasks;
 
 namespace BlackJack.Lib
 {
+    /// <summary>
+    /// Represents a standard deck of 52 playing cards.
+    /// </summary>
     public class CardDeck
     {
         public Card[] Cards { get; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CardDeck"/> class with a full set of 52 cards.
+        /// Creates one card for each combination of suit and value.
+        /// </summary>
         public CardDeck() 
         {
             Cards = new Card[52];
--- BlackJack.Lib/CardStack.cs
+++ BlackJack.Lib/CardStack.cs
@@ -7,13 +7,18 @@ using System.Threading.Tasks;
 namespace BlackJack.Lib
 {
     /// <summary>
-    /// 
+    /// Represents a stack of playing cards composed of multiple decks, providing functionality to shuffle and draw
+    /// cards.
     /// </summary>
     public class CardStack
     {
         public List<Card> Cards { get; }
 
         const int NUMBER_OF_DECKS = 6;
+
+        /// <summary>
+        /// Initializes a new instance of the CardStack class containing cards from multiple decks and shuffles them.
+        /// </summary>
         public CardStack()
         {
             Cards = new List<Card>();
@@ -25,6 +30,11 @@ namespace BlackJack.Lib
             Shuffle(3);
         }
 
+        /// <summary>
+        /// Randomizes the order of the cards in the collection by performing the specified number of shuffle passes.
+        /// </summary>
+        /// <param name="numberOfShuffles">The number of times to shuffle the card collection. Must be greater than zero; higher values increase the
+        /// degree of randomization.</param>
         public void Shuffle(int numberOfShuffles)
         {
             var rand = new Random();
@@ -40,6 +50,11 @@ namespace BlackJack.Lib
             }
 
         }
+
+        /// <summary>
+        /// Removes and returns the top card from the deck.
+        /// </summary>
+        /// <returns>The card that was at the top of the deck.</returns>
         public Card DrawCard()
         {
             var card = Cards[0];
