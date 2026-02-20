3da28b1872213859f521f4e73b329399f459f73d
Killernoob1163 <lileon2008@gmail.com>
2026-02-20 22:40:33 +0100

    CardStackTests added

---
+++ BlackJack.Lib.Tests/CardStackTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BlackJack.Lib.Tests
+{
+    [TestClass]
+    public class CardStackTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+            int expectedNumberOfCards = 312;
+            // Act
+            CardStack stack = new CardStack();
+            // Assert
+            Assert.AreEqual(expectedNumberOfCards, stack.Cards.Count); 
+
+        }
+    }
+}
